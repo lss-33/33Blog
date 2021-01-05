109c84e7a93423c477bb547bcada595c6f1bdb85
lishanshan <1549620779@qq.com>
2021-01-05 14:45:24 +0800

    初始化项目第三步

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,73 @@
+module.exports = {
+  "title": "33",
+  "description": "一个前端程序媛的博客",
+  // "base": '/blog/',
+  "dest": "docs/.vuepress/dist",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon1.png"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "Home",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitHub",
+            "link": "https://github.com/lss-33",
+            "icon": "reco-github"
+          }
+        ]
+      }
+    ],
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "Category"
+      },
+      "tag": {
+        "location": 3,
+        "text": "Tag"
+      }
+    },
+    "friendLink": [
+      {
+        "title": "欢迎你来到33博客",
+        "desc": "工友们，一起努力打工做人上人！",
+        "avatar": "https://github.com/lss-33",
+        "link": "https://github.com/lss-33"
+      }
+    ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "33",
+    "authorAvatar": "/avatar.png",
+    "record": "前端相关",
+    "startYear": "2020"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,10 @@
+---
+home: true
+heroText: 
+tagline: 
+bgImage: /hero.jpeg
+bgImageStyle: {
+  height: '450px'
+}
+isShowTitleInHome: false
+---
